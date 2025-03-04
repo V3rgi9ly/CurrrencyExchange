b3d2764d549a7b3ae3623193390581fdaa92b6b5
Sergey Starodubcev <starodubcev_2018@list.ru>
2025-03-05 00:50:56 +0200

    change class CurrenciesService

--- src/main/java/servlet/ServletCurrencies.java
+++ src/main/java/servlet/ServletCurrencies.java
@@ -7,13 +7,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Service.CurrenciesService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dto.CurrenciesDTO;
+
 
 @WebServlet("/currencies")
 public class ServletCurrencies extends HttpServlet {
 
+    private Gson gson = new Gson();
+    private final CurrenciesService currenciesService = new CurrenciesService();
+    private  List<CurrenciesDTO> currenciesDTO = new ArrayList<CurrenciesDTO>();
 
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try {
+            currenciesDTO=currenciesService.getCurrencies();
+            String employeeJsonString = this.gson.toJson(currenciesDTO);
+            PrintWriter out = response.getWriter();
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            out.print(employeeJsonString);
+            out.flush();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+
 
     }
 }
